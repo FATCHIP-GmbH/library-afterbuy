229c4b9e17c08ed7aa9ac3bd35f6f0cc44f27bd8
Stefan Mueller <stefstar@stefstar.de>
2021-06-01 12:43:50 +0200

    76280: added PR #28 to API Client

--- ApiClient.php
+++ ApiClient.php
@@ -261,6 +261,67 @@ class ApiClient
             $this->logger->debug('Request', array($request, $response));
         }
 
+        // BEGIN Advanced LogLevel  // added 2020-12-07 by XXL-Webdesgin
+        /*
+        Das erweiterte Debug-Logging wird über das Shopware Backend Log angezeigt (Shopware-Backend - Einstellungen - Logfile - Systemlog - aferbuy_production-[Datum].log)
+        Debuglevel:
+        0 - Alle Debug-Meldungen komplett deaktiviert.
+        1 - (Standard) nur Standard-Alive-Meldungen (werden angezeigt als Level "Error", Meldung "No Data recived" und Content "Orders, Read, Internal" -- aktuell ist es komplett deaktiviert / auskommentiert in der ReadOrdersService.php, da Variable in dieser Datei nicht übergeben werden kann)
+        2 - Standard-Alive Meldungen deaktiviert, Logging bei Fehlern (nur wenn der Aferbuy Response Success = 0 bzw. der Call-Status nicht successful ist)
+        3 - (Empfohlen) Standard-Alive Meldungen deaktiviert, Logging bei Fehlern, sowie Logging von Bestellübergaben an Aferbuy
+        4 - Standard-Alive Meldungen deaktiviert, Logging bei Fehlern, Logging von Bestellübergaben an Aferbuy sowie erweiterte Alive-Meldungen, dass der CronJob aktiv ist
+        Schöner wäre noch ein zusätzliches Parameterfeld ("Erweitertes Logging-Level" --> $advLogLevel) in der Plugin Konfiguration ("Verbesserungsvorschlag")
+        Dann könnte der User das Loglevel bequem über die Konfiguraton ändern.
+        EDIT: Wurde integriert - nur die Config muss noch korrekt ausgelesen werden, hab es aber nicht hin bekommen, die Funktion zum Auslesen der Config aufzurufen.
+        Hinweis: Folgende Datei wurde modifiziert, um das alte Logging zu deaktivieren:
+        [Shopware-Root]/custom/plugins/viaebShopwareAfterbuy/Services/ReadData/Internal/ReadOrdersService.php
+        (in v1.0.3 Zeile 89 auskommentiert)
+        */
+
+        $advLogLevel = 3;		// set level manually, because could not read from config at the moment.
+
+        // $this->config = $configReader->getByPluginName($pluginName);
+        // need to add parameters to this function to read $pluginName... How to do this?
+        // maybe add a check, if parameter "advLogLevel" is set correctly in the config, otherwise assign a default value and send an errormessage to the log...
+        // $advLogLevel = $this->config["advLogLevel"];
+
+        if ($advLogLevel >= 1) {
+            $requestFiltered = preg_replace('/<UserPassword>(.*?)<\/UserPassword>/', '<UserPassword>XXXXXXXX</UserPassword>', $request);				// remove Passwords
+            $requestFiltered = preg_replace('/<PartnerPassword>(.*?)<\/PartnerPassword>/', '<PartnerPassword>XXXXXXXX</PartnerPassword>', $requestFiltered);	// remove Passwords
+            $requestFiltered = preg_replace('/&UserPassword=(.*?)&Action=/', '&UserPassword=XXXXXXXXXX&Action=', $requestFiltered);					// remove Passwords
+            $requestFiltered = preg_replace('/&PartnerPass=(.*?)&PosAnz=/', '&PartnerPass=XXXXXXXX&PosAnz=', $requestFiltered);					// remove Passwords
+            $requestFiltered = str_replace(array("\r", "\n", "\t"), '', $requestFiltered);			// removes \n \r \t
+            $requestFiltered = preg_replace("/[\s][\s]*/", " ", $requestFiltered);				// removes double Whitespaces
+            $responseFiltered = str_replace(array("\r", "\n", "\t"), '', $response);			// removes \n \r \t
+            $responseFiltered = preg_replace("/[\s][\s]*/", " ", $responseFiltered);			// removes double Whitespaces
+
+            $statusText1 = '';
+            $statusText2 = 'Request and Response (XML)';
+            $content = array($requestFiltered, $responseFiltered, 'Shop-Doku (Parameter): https://xmldoku.afterbuy.de/shopdoku/', 'XML Doku: https://xmldoku.afterbuy.de/dokued/');
+
+            $xmlResponse = simplexml_load_string($responseFiltered);
+            if (($xmlResponse->CallStatus == "Success") OR ($xmlResponse->success == "1")) {
+                $needle = "&ArtikelStammID_1=";
+                if (strpos($requestFiltered, $needle) !== false){			// wenn der Request String "&ArtikelStammID=" enthält, handelt es sich um die Übergabe einer Bestellung.
+                    if ($advLogLevel >= 3) {
+                        $statusText1 = 'no Error - only info - Exported offer successfully to Afterbuy - ';
+                        $this->logger->error($statusText1 . $statusText2, $content);
+                    }
+                } else {
+                    if ($advLogLevel >= 4) {
+                        $statusText1 = 'no Error - only info - CronJob working correctly - ';
+                        $this->logger->error($statusText1 . $statusText2, $content);
+                    }
+                }
+            } else {									// if no Success in AB-Response...
+                if ($advLogLevel >= 2) {
+                    $statusText1 = 'ERROR! Check ';
+                    $this->logger->error($statusText1 . $statusText2, $content);
+                }
+            }
+        }
+        // END Advanced Log
+
         return $this->serializer->decode($response, 'response/xml');
     }
 
